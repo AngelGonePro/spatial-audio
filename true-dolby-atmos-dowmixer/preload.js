5a3c646f50e5a51a4b40e717eaa257c0203a11e9
AngelGonePro <97822157+AngelGonePro@users.noreply.github.com>
2026-05-12 15:58:32 -0600

    Delete true-dolby-atmos-dowmixer directory

--- true-dolby-atmos-dowmixer/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getPath: (file) => file.path,
-  runFFmpeg: (payload) => ipcRenderer.invoke('run-ffmpeg', payload)
-});
