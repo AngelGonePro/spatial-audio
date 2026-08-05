8bd1cf67477abadb351f84472cfa9a5f9e7e8587
AngelGonePro <97822157+AngelGonePro@users.noreply.github.com>
2026-08-05 02:32:17 -0600

    Update preload.js
    Add progress bar

--- full-mixer-use-this-one/preload.js
+++ full-mixer-use-this-one/preload.js
@@ -3,6 +3,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
   getPath: (file) => file.path,
   runFFmpeg: (payload) => ipcRenderer.invoke('run-ffmpeg', payload),
+  runFFmpegWithProgress: (payload) => ipcRenderer.invoke('run-ffmpeg-with-progress', payload),
+  onFFmpegProgress: (callback) => ipcRenderer.on('ffmpeg-progress', (event, data) => callback(data)),
   getChannelCount: (payload) => ipcRenderer.invoke('get-channel-count', payload),
   getDuration: (payload) => ipcRenderer.invoke('get-duration', payload)
 });
